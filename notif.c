1527f9f035d8298ae40ce1241aa3a6f073340a3e
Daniel Maxime <maxux.unix@gmail.com>
2011-11-21 23:44:48 +0100

    Blink on new message, date fixed (%02d), bug fix

--- notif.c
+++ notif.c
@@ -4,6 +4,7 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include <signal.h>
+#include <unistd.h>
 #include <time.h>
 
 #include "notif.h"
@@ -15,7 +16,7 @@ void print_line(char *data) {
 	time(&rawtime);
 	timeinfo = localtime(&rawtime);
 	
-	printw("[%d:%d:%d] %s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, data);
+	printw("[%02d:%02d:%02d] %s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, data);
 	refresh();
 }
 
@@ -29,10 +30,12 @@ void trim_text(char *data) {
 }
 
 void dummy(int signal) {
-	
+	switch(signal) {
+		// Nothing yet
+	}
 }
 
-void mark_as_read() {
+void mark_as_read(int *newmsg) {
 	int i;
 	
 	while(1) {
@@ -43,17 +46,38 @@ void mark_as_read() {
 			printw("-");
 		
 		printw("\n");
+		
 		bkgd(COLOR_PAIR(2));
 		refresh();
-		bkgd(COLOR_PAIR(1));
+		
+		*newmsg = 0;
 	}
 }
 
-int main(int argc, const char *argv[]) {
+void blink(void) {
+	int i;
+	
+	usleep(200000);
+	
+	for(i = 0; i < 15; i++) {
+		bkgd(COLOR_PAIR(2));
+		refresh();
+		
+		usleep(100000);
+		
+		bkgd(COLOR_PAIR(1));
+		refresh();
+		
+		usleep(100000);
+	}
+}
+
+int main(void) {
 	int i;
 	char buffer[1024];
 	FILE *fp;
-	pthread_t waitkey;
+	pthread_t waitkey, blinkwin;
+	char newmsg;
 	
 	/* Init curses */
 	initscr();		/* Init ncurses */
@@ -62,33 +86,38 @@ int main(int argc, const char *argv[]) {
 	start_color();		/* Enable color */
 	curs_set(0);		/* Disable cursor */
 	keypad(stdscr, TRUE);
+	
+	/* Init Scroll */
+	scrollok(stdscr, 1);
+	
+	/* Skipping Resize Signal */
+	signal(SIGWINCH, dummy);
 
 	/* Init Colors */
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
 	init_pair(2, COLOR_WHITE, COLOR_BLACK);
 	
-	attron(COLOR_PAIR(1));
-	bkgd(COLOR_PAIR(1));
+	attron(COLOR_PAIR(2));
+	bkgd(COLOR_PAIR(2));
 	
-	/* State Message */
-	attron(COLOR_PAIR(1));
-	attron(A_BOLD);
-	
-	scrollok(stdscr, 1);
-	
+	/* State Message */	
 	for(i = 0; i < getmaxy(stdscr); i++)
 		printw("\n");
-		
+	
+	/* Starting with 'no news messages' */
+	attron(COLOR_PAIR(2));
+	attron(A_BOLD);	
 	print_line("Waiting notifications...");
 	attroff(A_BOLD);
 	
+	/* Waiting new messages */
 	attron(COLOR_PAIR(1));
-	
-	signal(SIGWINCH, dummy);
-	
 	refresh();
 	
-	if(pthread_create(&waitkey, NULL, (void *) mark_as_read, (void*) NULL) != 0)
+	/* Init Message Count */
+	newmsg = 0;
+	
+	if(pthread_create(&waitkey, NULL, (void *) mark_as_read, (void*) &newmsg) != 0)
 		return 1;
 	
 	while(1) {
@@ -101,8 +130,16 @@ int main(int argc, const char *argv[]) {
 		
 		/* Waiting News */
 		while(fgets(buffer, sizeof(buffer), fp) != NULL) {
+			if(!newmsg) {
+				if(pthread_create(&blinkwin, NULL, (void *) blink, (void*) NULL) != 0) {
+					return 1;
+				}
+			}
+				
 			trim_text(buffer);
 			print_line(buffer);
+			
+			newmsg++;
 		}
 	
 		fclose(fp);
