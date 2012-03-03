9c36bb0800de76a8b5b0b85272c8568bb2f30618
Daniel Maxime <maxux.unix@gmail.com>
2012-03-03 21:43:02 +0100

    [suckless] added usage, spelling error fixed, tabs
    From: Christoph Lohmann
    
    0001-Adding-usage-and-a-notification-file-parameter.patch
    This will make it possible to use `easynotif /some/fifo`.
    
    0002-Removing-spurious-tabs.patch
    This patch is removing spurious tabs.
    
    0003-Fixing-a-spelling-error.patch
    This one will just fix a spelling error.

--- notif.c
+++ notif.c
@@ -7,24 +7,22 @@
 #include <unistd.h>
 #include <time.h>
 
-#include "notif.h"
-
 void print_line(char *data) {
 	time_t rawtime;
 	struct tm * timeinfo;
-	
+
 	time(&rawtime);
 	timeinfo = localtime(&rawtime);
-	
+
 	printw("[%02d:%02d:%02d] %s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, data);
 	refresh();
 }
 
 void trim_text(char *data) {
 	int len;
-	
+
 	len = strlen(data);
-	
+
 	if(data[len - 1] == '\n')
 		data[len - 1] = '\0';
 }
@@ -37,30 +35,42 @@ void dummy(int signal) {
 
 void mark_as_read(int *newmsg) {
 	int i;
-	
+
 	while(1) {
 		getchar();
-		
+
 		i = getmaxx(stdscr);
 		while(i-- > 1)
 			printw("-");
-		
+
 		printw("\n");
-		
+
 		bkgd(COLOR_PAIR(2));
 		refresh();
-		
+
 		*newmsg = 0;
 	}
 }
 
-int main(void) {
+void usage(char *argv0) {
+	fprintf(stderr, "usage: %s [-h] file\n", argv0);
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
 	int i;
-	char buffer[1024];
+	char buffer[1024], *notiffile;
 	FILE *fp;
 	pthread_t waitkey;
 	char newmsg;
-	
+
+	if(argc < 2)
+		usage(argv[0]);
+	if(!strcmp(argv[1], "-h"))
+		usage(argv[0]);
+
+	notiffile = argv[1];
+
 	/* Init curses */
 	initscr();		/* Init ncurses */
 	cbreak();		/* No break line */
@@ -68,10 +78,10 @@ int main(void) {
 	start_color();		/* Enable color */
 	curs_set(0);		/* Disable cursor */
 	keypad(stdscr, TRUE);
-	
+
 	/* Init Scroll */
 	scrollok(stdscr, 1);
-	
+
 	/* Skipping Resize Signal */
 	signal(SIGWINCH, dummy);
 
@@ -79,62 +89,63 @@ int main(void) {
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
 	init_pair(2, COLOR_WHITE, COLOR_BLACK);
 	init_pair(3, COLOR_YELLOW, COLOR_BLUE);
-	
+
 	attron(COLOR_PAIR(2));
 	bkgd(COLOR_PAIR(2));
-	
-	/* State Message */	
+
+	/* State Message */
 	for(i = 0; i < getmaxy(stdscr); i++)
 		printw("\n");
-	
+
 	/* Starting with 'no news messages' */
 	attron(COLOR_PAIR(2));
-	attron(A_BOLD);	
-	print_line("Waiting notifications...");
+	attron(A_BOLD);
+	print_line("Waiting for notifications...");
 	attroff(A_BOLD);
-	
+
 	/* Waiting new messages */
 	attron(COLOR_PAIR(1));
 	refresh();
-	
+
 	/* Init Message Count */
 	newmsg = 0;
-	
+
 	if(pthread_create(&waitkey, NULL, (void *) mark_as_read, (void*) &newmsg) != 0)
 		return 1;
-	
+
 	while(1) {
 		/* Init Files */
-		fp = fopen(NOTIF_FILE, "r");
+		fp = fopen(notiffile, "r");
 		if(!fp) {
+			endwin();
 			perror("fopen");
 			return 1;
 		}
-		
+
 		/* Waiting News */
 		while(fgets(buffer, sizeof(buffer), fp) != NULL) {
 			if(!newmsg) {
 				/* Background blue */
 				bkgd(COLOR_PAIR(1));
 				refresh();
-				
-				/* Beep */			
+
+				/* Beep */
 				printf("\x07");
 			}
-			
+
 			/* New Message */
 			attron(COLOR_PAIR(3));
 			attron(A_BOLD);
-				
+
 			trim_text(buffer);
 			print_line(buffer);
-			
+
 			newmsg++;
 		}
-	
+
 		fclose(fp);
 	}
-	
+
 	endwin();
 
 	return 0;
--- notif.h
+++
@@ -1,5 +0,0 @@
-#ifndef __NOTIF_H
-	#define __NOTIF_H
-	
-	#define NOTIF_FILE	"/tmp/notif.stack"
-#endif
